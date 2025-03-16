f4217173de03ff33863b6dc24b96f28511c72daa
Kaitlyn Curtis <kaitlyn.ann.curtis@gmail.com>
2025-03-15 23:18:14 -0400

    updated to compile for qnx but doesn't render

--- OpenGLTest2/main5.cpp
+++ OpenGLTest2/main5.cpp
@@ -5,11 +5,17 @@
 // Window size
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
+void check_is_true(EGLBoolean rc, char* msg){
+ if(rc != EGL_TRUE){
+	fprintf(stderr, "failure %s\n", msg);
+	exit(1);
+  }
+}
 int main() {
     // Initialize EGL display
     EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-    eglInitialize(display, NULL, NULL);
+    EGLBoolean rc =     eglInitialize(display, NULL, NULL);
+    check_is_true(rc, "initialize");
 
     // Choose an EGL config
     EGLConfig config;
@@ -19,15 +25,21 @@ int main() {
         EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT,
         EGL_NONE
     };
-    eglChooseConfig(display, configAttribs, &config, 1, &numConfigs);
-
-    // Create an EGL rendering context
-    EGLContext context = eglCreateContext(display, config, EGL_NO_CONTEXT, (EGLint[]){
+    rc = eglChooseConfig(display, configAttribs, &config, 1, &numConfigs);
+	check_is_true(rc, "choose config");
+    
+EGLint context_data[] = {
         EGL_CONTEXT_CLIENT_VERSION, 3, EGL_NONE
-    });
-
+    };    
+    // Create an EGL rendering context
+    EGLContext context = eglCreateContext(display, config, EGL_NO_CONTEXT, context_data);
+	
     // Create a window surface (For Native Window Replace 0)
     EGLSurface surface = eglCreateWindowSurface(display, config, (EGLNativeWindowType)0, NULL);
+    if(surface == NULL || context == NULL){
+	fprintf(stderr, "surface/context was null %d", surface==NULL);
+	exit(1);
+}
     eglMakeCurrent(display, surface, surface, context);
 
     // Main loop
